2e3b0082ad6ba7e22ccf4295e196ecfc232b3149
Deepak <deepakjgrt99@gmail.com>
2025-05-18 10:15:51 +0530

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Deepak Kumar Behera on 18/05/25.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GuessTheFlagTests/GuessTheFlagTests.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagTests.swift
+//  GuessTheFlagTests
+//
+//  Created by Deepak Kumar Behera on 18/05/25.
+//
+
+import Testing
+@testable import GuessTheFlag
+
+struct GuessTheFlagTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
